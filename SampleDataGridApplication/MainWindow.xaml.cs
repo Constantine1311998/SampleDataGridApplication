656666fcc0b4d52e2f0e3f0ece1bd6ac56930956
ShibinSibyVarghese <constantine1311998@gmail.com>
2023-06-14 23:53:19 +0530

    Experimental Features.

--- SampleDataGridApplication/MainWindow.xaml.cs
+++ SampleDataGridApplication/MainWindow.xaml.cs
@@ -13,11 +13,29 @@ namespace SampleDataGridApplication
             InitializeComponent();
             this.DataContext = new MainViewModel();
         }
-
+        /// <summary>
+        /// When text box looses focus this will fire lost focus command.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             var vm = this.DataContext as MainViewModel;
             vm.LostFocusCommand.Execute(sender);
+        }
+
+        /// <summary>
+        /// Fires lost focus command when Enter key is hit.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void filePathTxtBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                var vm = this.DataContext as MainViewModel;
+                vm.LostFocusCommand.Execute(sender);
+            }
         }
     }
 }
